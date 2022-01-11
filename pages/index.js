aa6e1a509ef942504e14fb5a68bbd2e82d34fe85
Danny Doan <doandk@uci.edu>
2022-01-10 18:54:56 -0800

    small changes

--- pages/index.js
+++ pages/index.js
@@ -17,9 +17,9 @@ export default function Home() {
         <main className="flex-grow">
           <div className="flex justify-end m-4 md:m-8 lg:m-16">
             <ul className="space-x-5 md:space-x-10 lg-space-x-20">
-              <li className="inline"><a href="#" className="text-white text-xl md:text-2xl lg:text-4xl">/home</a></li>
-              <li className="inline"><a href="#" className="text-white text-xl md:text-2xl lg:text-4xl">/projects</a></li>
-              <li className="inline"><a href="#" className="text-white text-xl md:text-2xl lg:text-4xl">/interests</a></li>
+              <li className="inline"><a href="#" className=" underline font-semibold text-white text-xl md:text-2xl lg:text-4xl">/home</a></li>
+              <li className="inline"><a href="#" className="hover:font-semibold text-white text-xl md:text-2xl lg:text-4xl">/projects</a></li>
+              <li className="inline"><a href="#" className="hover:font-semibold text-white text-xl md:text-2xl lg:text-4xl">/interests</a></li>
             </ul>
           </div>
 
@@ -32,10 +32,10 @@ export default function Home() {
             </p4>
 
             <ul className="text-xl pt-4 md:pt-8 lg-12 md:text-2xl lg:text-4xl text-white pl-2 md:pl-10 lg:pl-30 xl:pl-40 
-            space-x-4 md:space-x-8 lg-space-x-16 underline">
-              <li className="inline"><a href="mailto:dannykdoan@gmail.com">email➚</a></li>
-              <li className="inline"><a href="https://github.com/dkdoan">github➚</a></li>
-              <li className="inline"><a href="https://www.linkedin.com/in/danny-doan-609595223">linkedin➚</a></li>
+            space-x-4 md:space-x-8 lg-space-x-16">
+              <li className="inline hover:font-semibold"><a href="mailto:dannykdoan@gmail.com">email➚</a></li>
+              <li className="inline hover:font-semibold"><a href="https://github.com/dkdoan">github➚</a></li>
+              <li className="inline hover:font-semibold"><a href="https://www.linkedin.com/in/danny-doan-609595223">linkedin➚</a></li>
             </ul>
           </div>
 
