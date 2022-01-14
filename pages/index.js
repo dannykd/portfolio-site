e2a3a998984d8c1b5c043b5eb6c6eb6d4459308f
Danny Doan <doandk@uci.edu>
2022-01-13 23:58:38 -0800

    new design and projects page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 
 
@@ -17,23 +18,23 @@ export default function Home() {
         <main className="flex-grow">
           <div className="flex justify-end m-4 md:m-8 lg:m-16">
             <ul className="space-x-5 md:space-x-10 lg-space-x-20">
-              <li className="inline"><a href="#" className=" underline font-semibold text-white text-xl md:text-2xl lg:text-4xl">/home</a></li>
-              <li className="inline"><a href="#" className="hover:font-semibold text-white text-xl md:text-2xl lg:text-4xl">/projects</a></li>
-              <li className="inline"><a href="#" className="hover:font-semibold text-white text-xl md:text-2xl lg:text-4xl">/interests</a></li>
+              <li className="inline"><Link href="/"><a className="underline hover:font-semibold text-white text-xl md:text-2xl lg:text-4xl">/home</a></Link></li>
+              <li className="inline"><Link href="/projects"><a className="hover:font-semibold text-white text-xl md:text-2xl lg:text-4xl">/projects</a></Link></li>
+              <li className="inline"><Link href="#"><a className="hover:font-semibold text-white text-xl md:text-2xl lg:text-4xl">/interests</a></Link></li>
             </ul>
           </div>
 
           <div className="flex flex-col ">
             <h1 className="pt-10 text-3xl md:text-5xl lg:text-6xl text-white pl-2 md:pl-10 lg:pl-30 xl:pl-40">
-              hey, i&apos;m danny.    
+              Hey, i&apos;m Danny.    
             </h1>
-            <p4 className="text-xl pt-4 md:pt-8 lg-12 md:text-2xl lg:text-4xl text-white pl-2 md:pl-10 lg:pl-30 xl:pl-40">
-              20y/o california based developer studying computer science @uci.
+            <p4 className="ml-6 text-gray-400 text-xl pt-4 md:pt-8 lg-12 md:text-2xl lg:text-4xl text-white pl-2 md:pl-10 lg:pl-30 xl:pl-40">
+              20 year-old developer based in California. Currently studying Computer Science @UCI.
             </p4>
 
             <ul className="text-xl pt-4 md:pt-8 lg-12 md:text-2xl lg:text-4xl text-white pl-2 md:pl-10 lg:pl-30 xl:pl-40 
             space-x-4 md:space-x-8 lg-space-x-16">
-              <li className="inline hover:font-semibold"><a href="mailto:dannykdoan@gmail.com">email➚</a></li>
+              <li className="inline hover:font-semibold" ><a href="mailto:dannykdoan@gmail.com">email➚</a></li>
               <li className="inline hover:font-semibold"><a href="https://github.com/dkdoan">github➚</a></li>
               <li className="inline hover:font-semibold"><a href="https://www.linkedin.com/in/danny-doan-609595223">linkedin➚</a></li>
             </ul>
@@ -41,8 +42,9 @@ export default function Home() {
 
           <div className="pt-20 md:pt-24 lg:pt-28">
             <p1 className="text-l pt-4 md:pt-8 lg-12 md:text-2xl lg:text-4xl text-white pl-2 md:pl-10 lg:pl-30 xl:pl-40">
-              also, here&apos;s a picture of my cat &lt;3</p1>
-            <img className="pt-4 h-72 pl-2 md:pl-10 lg:pl-30 xl:pl-40" src="/rue_for_site.jpg"/>
+
+            </p1>
+            
           </div>
         </main>
         <footer>
---
+++ pages/projects.js
@@ -0,0 +1,73 @@
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+import Head from 'next/head'
+function Projects() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Danny Doan</title>
+        <meta name="author" content="Danny Doancdcdgit addgi-" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <body className="bg-black flex flex-col min-h-screen">
+        <main className="flex-grow">
+          <div className="flex justify-end m-4 md:m-8 lg:m-16">
+            <ul className="space-x-5 md:space-x-10 lg-space-x-20">
+              <li className="inline"><Link href="/"><a className="hover:font-semibold text-white text-xl md:text-2xl lg:text-4xl">/home</a></Link></li>
+              <li className="inline"><Link href="/projects"><a className="underline hover:font-semibold text-white text-xl md:text-2xl lg:text-4xl">/projects</a></Link></li>
+              <li className="inline"><Link href="#"><a className="hover:font-semibold text-white text-xl md:text-2xl lg:text-4xl">/interests</a></Link></li>
+            </ul>
+          </div>
+          <div clasName="flex flex-col">
+            <div className="flex mt-8 md:mt-16">
+                        <div class="max-w-xl rounded overflow-hidden shadow-lg ml-8 md:ml-48 mb-8 md:mb-12">
+                            <div class="font-bold text-xl md:text-3xl mb-2 text-white rounded-lg">Study Session Manager</div>
+                                <p class="text-gray-400 text-l md:text-2xl pb-2">
+                                 A full-stack web application that allows users to start/stop timed study sessions. Users can create an account and log their study sessions.
+                                </p>
+                            
+                                <span class="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#python</span>
+                                <span class="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">django</span>
+                                <span class="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#html/css</span>
+                            </div>
+                        </div>
+
+                        <div class="max-w-xl rounded overflow-hidden shadow-lg ml-8 md:ml-48 mb-8 md:mb-12">
+                            <div class="font-bold text-xl md:text-3xl mb-2 text-white rounded-lg">Placeholder</div>
+                                <p class="text-gray-400 text-l md:text-2xl pb-2">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                                </p>
+                            
+                                <span class="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                                <span class="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                                <span class="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                            </div>
+                        
+
+                        <div class="max-w-xl rounded overflow-hidden shadow-lg ml-8 md:ml-48 mb-8 md:mb-12">
+                            <div class="font-bold text-xl md:text-3xl mb-2 text-white rounded-lg">Something Cool and Random</div>
+                                <p class="text-gray-400 text-l md:text-2xl pb-2">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                                </p>
+                            
+                                <span class="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                                <span class="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                                <span class="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                            </div>
+            </div>       
+                    
+
+          
+
+        </main>
+        <footer>
+          <p className="text-white text-sm md:text-md text-center">-------built with nextjs + tailwind-------</p>
+        </footer>
+      </body>
+  
+    </div>
+  )
+}
+
+export default Projects
