81f988bd798556cde90beea74d79fbdbc6a79845
Danny Doan <doandk@uci.edu>
2022-01-14 17:40:41 -0800

    added links

--- pages/projects.js
+++ pages/projects.js
@@ -21,8 +21,12 @@ function Projects() {
           </div>
           <div clasName="flex flex-col">
             <div className="flex mt-8 md:mt-16">
+
                         <div className="max-w-xl rounded overflow-hidden shadow-lg ml-8 md:ml-48 mb-8 md:mb-12 pt-8">
-                            <div className="font-bold text-xl md:text-3xl mb-2 text-white rounded-lg">Study Session Manager</div>
+                            <div className="font-bold text-xl md:text-3xl mb-1 text-white rounded-lg">Study Session Manager</div>
+                                <a href="https://skolr.herokuapp.com/" className="hover:font-semibold text-sky-400 text-m md:text-2xl pb-2">
+                                https://skolr.herokuapp.com/➚ 
+                                </a>
                                 <p className="text-gray-400 text-l md:text-2xl pb-2">
                                  A full-stack web application that allows users to start/stop timed study sessions. Users can create an account and log their study sessions.
                                 </p>
@@ -34,7 +38,10 @@ function Projects() {
                         </div>
 
                         <div className="max-w-xl rounded overflow-hidden shadow-lg ml-8 md:ml-48 mb-8 md:mb-12">
-                            <div className="font-bold text-xl md:text-3xl mb-2 text-white rounded-lg">Placeholder</div>
+                            <div className="font-bold text-xl md:text-3xl mb-1 text-white rounded-lg">Placeholder</div>
+                                <a href="https://skolr.herokuapp.com/" className="hover:font-semibold text-sky-400 text-m md:text-2xl pb-2">
+                                https://test.herokuapp.com/➚ 
+                                </a>
                                 <p className="text-gray-400 text-l md:text-2xl pb-2">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                                 </p>
@@ -45,16 +52,6 @@ function Projects() {
                             </div>
                         
 
-                        <div className="max-w-xl rounded overflow-hidden shadow-lg ml-8 md:ml-48 mb-8 md:mb-12">
-                            <div className="font-bold text-xl md:text-3xl mb-2 text-white rounded-lg">Something Cool and Random</div>
-                                <p className="text-gray-400 text-l md:text-2xl pb-2">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                                </p>
-                            
-                                <span className="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                                <span className="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                                <span className="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-                            </div>
             </div>       
                     
 
